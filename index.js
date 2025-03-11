5c4c3b3f9f6b3ef8a172cda2e1f62bc323d38d35
Gajanan Vigneswaran <gajanan.vig@gmail.com>
2025-03-11 00:58:21 -0400

    added Vercel origin

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const port = 5000;
 
 const allowedOrigins = [
     'http://localhost:5173', // Local development
-    'https://portfolio-blog-frontend.vercel.app', // Your Vercel deployment
+    'https://portfolio-blog-frontend.vercel.app', // Vercel deployment
+    'https://portfolio-blog-frontend-git-main-gajanans-projects-38fbb10a.vercel.app/', // Vercel deployment
     'https://www.gajanan.live/', // Your custom domain
     'https://gajanan.live/', // Your custom domain
     'https://www.gajanan.live', // Your custom domain
